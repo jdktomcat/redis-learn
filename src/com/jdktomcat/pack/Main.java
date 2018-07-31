cd00a9c0815b2459919b5492d481c123dbb5cfd3
汤旗 <tangqihlw@bbktel.com>
2018-07-31 11:24:05 +0800

    源码注释

--- src/com/jdktomcat/pack/Main.java
+++ src/com/jdktomcat/pack/Main.java
@@ -3,6 +3,8 @@ package com.jdktomcat.pack;
 import com.jdktomcat.pack.bean.Person;
 import com.jdktomcat.pack.util.HashMap;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,5 +19,9 @@ public class Main {
         }
         Map<Person, Integer> otherMap = new HashMap<Person, Integer>(map);
         System.out.println(otherMap.size());
+
+        List<String> list = new ArrayList<>();
+
+
     }
 }
