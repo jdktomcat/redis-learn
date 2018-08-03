e4987568d8fbe3829cbb8f8825085d0de51d5dfb
汤旗 <tangqihlw@bbktel.com>
2018-08-03 17:53:33 +0800

    JVM类加载

---
+++ src/com/jdktomcat/pack/bytecode/Test.java
@@ -0,0 +1,23 @@
+package com.jdktomcat.pack.bytecode;
+
+/**
+ * 类描述：
+ *
+ * @author 汤旗
+ * @date 2018-08-03
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("345");
+    }
+
+    public void m1() {
+        System.out.println("m1");
+    }
+
+    public void m2() {
+        System.out.println("m2");
+    }
+}
