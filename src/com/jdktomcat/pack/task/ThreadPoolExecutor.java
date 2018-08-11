1417a6e1550b31fa387c1aef0eb2423beb53828c
jdktomcat <jdktomcat@163.com>
2018-08-11 17:31:33 +0800

    线程池文档

--- src/com/jdktomcat/pack/task/ThreadPoolExecutor.java
+++ src/com/jdktomcat/pack/task/ThreadPoolExecutor.java
@@ -225,9 +225,13 @@ public class ThreadPoolExecutor extends AbstractExecutorService {
      * runState is stored in the high-order bits
      */
     private static final int RUNNING = -1 << COUNT_BITS;
+
     private static final int SHUTDOWN = 0 << COUNT_BITS;
+
     private static final int STOP = 1 << COUNT_BITS;
+
     private static final int TIDYING = 2 << COUNT_BITS;
+
     private static final int TERMINATED = 3 << COUNT_BITS;
 
     // Packing and unpacking ctl
