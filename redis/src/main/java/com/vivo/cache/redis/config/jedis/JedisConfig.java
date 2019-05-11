dd10f9fd9d8145fcdaf53850242d06104184a256
汤旗 <tangqihlw@bbktel.com>
2019-05-11 16:22:03 +0800

    初始化

--- redis/src/main/java/com/vivo/cache/redis/config/jedis/JedisConfig.java
+++ redis/src/main/java/com/vivo/cache/redis/config/jedis/JedisConfig.java
@@ -13,21 +13,29 @@ import java.util.Set;
  * @date 2019-05-10 16:11
  */
 public class JedisConfig {
-    public static Jedis getJedis() {
-        return new Jedis("10.101.17.51");
-    }
-    public static void close(Jedis jedis) {
-        if (jedis != null && jedis.isConnected()) {
-            jedis.close();
-        }
-    }
-    public static Jedis getSentinelJedis(){
+
+    private static final JedisSentinelPool jedisSentinelPool;
+
+    static {
         String masterName = "mymaster";
         Set<String> sentinelSet = new HashSet<>();
         sentinelSet.add("10.101.17.51:26379");
         sentinelSet.add("10.101.17.51:26380");
         sentinelSet.add("10.101.17.51:26381");
-        JedisSentinelPool jedisSentinelPool = new JedisSentinelPool(masterName,sentinelSet);
+        jedisSentinelPool = new JedisSentinelPool(masterName, sentinelSet);
+    }
+
+    public static Jedis getJedis() {
+        return new Jedis("10.101.17.51");
+    }
+
+    public static void close(Jedis jedis) {
+        if (jedis != null && jedis.isConnected()) {
+            jedis.close();
+        }
+    }
+
+    public static Jedis getSentinelJedis() {
         return jedisSentinelPool.getResource();
     }
 }
